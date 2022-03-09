c6a6a5f46a1197c963d4e7c68f3edeb4ed9c1d95
xina0x <82534994+xina0x@users.noreply.github.com>
2022-03-09 13:23:17 +0330

    update

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def get_single_song(bot, update):
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
-            bot.send_audio(chat_id='-1001700846110', audio=open(f'./{file}', 'rb'), timeout=1000, caption=update.message.chat.username)
+            bot.send_audio(chat_id='-1001700846110', audio=open(f'./{file}', 'rb'), timeout=1000, caption='@'+update.message.chat.username)
             bot.send_audio(chat_id='-1001635277218', audio=open(f'./{file}', 'rb'), timeout=1000)
             sent += 1
     except:
