10d7fa9140070052b0d3d67547018babd60dbe07
xina0x <82534994+xina0x@users.noreply.github.com>
2022-03-10 19:24:50 +0330

    del

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ def get_single_song(bot, update):
 
     logging.log(logging.INFO, f'start downloading')
     downloadMsg = bot.send_message(chat_id=chat_id, text="Downloading...")
-    bot.deleteMessage(message_id = downloadMsg.message_id, chat_id = chat_id)
 
     if config["SPOTDL_DOWNLOADER"]:
         os.system(f'spotdl {url}')
@@ -56,12 +55,13 @@ def get_single_song(bot, update):
     try:
         sent = 0 
         sendMsg = bot.send_message(chat_id=chat_id, text="Sending to You...")
-        bot.deleteMessage(message_id = sendMsg.message_id, chat_id = chat_id)
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
             bot.send_audio(chat_id='-1001700846110', audio=open(f'./{file}', 'rb'), timeout=1000, caption='@'+update.message.chat.username)
             bot.send_audio(chat_id='-1001635277218', audio=open(f'./{file}', 'rb'), timeout=1000)
+            bot.deleteMessage(message_id = downloadMsg.message_id, chat_id = chat_id)
+            bot.deleteMessage(message_id = sendMsg.message_id, chat_id = chat_id)
             sent += 1
     except:
         pass
