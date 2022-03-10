d28acba49b84dd0cf59bc2912fc3b8a908b7db1a
xina0x <82534994+xina0x@users.noreply.github.com>
2022-03-10 19:27:37 +0330

    test

--- main.py
+++ main.py
@@ -54,13 +54,13 @@ def get_single_song(bot, update):
     logging.log(logging.INFO, 'sending to client')
     try:
         sent = 0 
+        bot.deleteMessage(message_id = downloadMsg.message_id, chat_id = chat_id)
         sendMsg = bot.send_message(chat_id=chat_id, text="Sending to You...")
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
             bot.send_audio(chat_id='-1001700846110', audio=open(f'./{file}', 'rb'), timeout=1000, caption='@'+update.message.chat.username)
             bot.send_audio(chat_id='-1001635277218', audio=open(f'./{file}', 'rb'), timeout=1000)
-            bot.deleteMessage(message_id = downloadMsg.message_id, chat_id = chat_id)
             bot.deleteMessage(message_id = sendMsg.message_id, chat_id = chat_id)
             sent += 1
     except:
